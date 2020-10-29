d4240459c297e4650679443cc18180ebea6086ed
technicallyty <48813565+tytech3@users.noreply.github.com>
2020-10-29 15:36:16 -0700

    clean
    cleanup

--- App.py
+++ App.py
@@ -2,16 +2,21 @@ import sys
 import random
 from PySide2 import QtCore, QtWidgets, QtGui
 
+"""
+class ModuleSelectorWidget(QtWidgets.QWidget):
+    def __init__(self, parent=HomeWidget):
+        super(ModuleSelectorWidget, self).__init__(parent)
+        """
 
 class PlayWidget(QtWidgets.QWidget):
-    def __init__(self, operand):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(PlayWidget, self).__init__(parent)
 
-        self.operand = operand
+        self.operand = '-'
         self.random = ['1','2','3','4','5','6','7','8','9','10']
 
-        textStyle = ("color: white; font-size: 30px;")
-        solStyle = ("color: red; font-size: 35px;")
+        textStyle = ("color: white; font-size: 60px;")
+        solStyle = ("color: red; font-size: 65px;")
 
         # question widget
         self.numLeft = QtWidgets.QLabel(random.choice(self.random))
@@ -33,67 +38,69 @@ class PlayWidget(QtWidgets.QWidget):
         self.QuestionLayout.addWidget(self.numRight)
         self.QuestionLayout.addWidget(self.eq)
         self.QuestionLayout.addWidget(self.sol)
-        #  apply layout
-        #self.setLayout(self.QuestionLayout)
 
+        #   input section
         self.TextInput = QtWidgets.QLineEdit()
         self.TextInput.setStyleSheet("color: white; padding: 30px 0px; font-size: 35px;")
         self.TextInput.returnPressed.connect(self.operate)
         self.submitButton = QtWidgets.QPushButton("Submit")
         self.submitButton.setStyleSheet("background-color: green; color: white; padding: 15px 15px; font-size: 20px; border-radius: 10px;")
 
+        #   input layout
         self.inputLayout = QtWidgets.QHBoxLayout()
         self.inputLayout.addWidget(self.TextInput)
         self.inputLayout.addWidget(self.submitButton)
         self.submitButton.clicked.connect(self.operate)
-        #self.setLayout(self.inputLayout)
 
 
+        #   layout all widgets
         self.stack = QtWidgets.QVBoxLayout()
         self.stack.addLayout(self.QuestionLayout)
         self.stack.addLayout(self.inputLayout)
         self.setLayout(self.stack)
     
     def shuffle(self):
-        self.numLeft.setText(random.choice(self.random))
-        self.numRight.setText(random.choice(self.random))
+
+        right = random.choice(self.random)
+        left = random.choice(self.random)
+
+        if self.operand == '-':
+            if int(right) > int(left):
+                temp = right
+                right = left
+                left = temp
+
+        self.numLeft.setText(left)
+        self.numRight.setText(right)
         self.numLeft.repaint()
         self.numRight.repaint()
     
     def operate(self):
         if(self.operand == "+"):
             if ((int(self.numLeft.text()) + int(self.numRight.text())) != int(self.TextInput.text())):
-                print("rip")
                 self.showMessageBox("Incorrect, please try again.")
             else:
-                print("nice")
                 self.showMessageBox("Nice job! you got it!")
                 self.TextInput.clear()
                 self.shuffle()
         elif(self.operand == "-"):
             if ((int(self.numLeft.text()) - int(self.numRight.text())) != int(self.TextInput.text())):
-                print("rip")
                 self.showMessageBox("Incorrect, please try again.")
             else:
-                print("nice")
                 self.showMessageBox("Nice job! you got it!")
                 self.TextInput.clear()
                 self.shuffle()
         elif(self.operand == "*"):
             if ((int(self.numLeft.text()) * int(self.numRight.text())) != int(self.TextInput.text())):
-                print("rip")
                 self.showMessageBox("Incorrect, please try again.")
             else:
-                print("nice")
                 self.showMessageBox("Nice job! you got it!")
                 self.TextInput.clear()
                 self.shuffle()
         elif(self.operand == "/"):
             if ((int(self.numLeft.text()) / int(self.numRight.text())) != int(self.TextInput.text())):
-                print("rip")
                 self.showMessageBox("Incorrect, please try again.")
             else:
-                print("nice")
                 self.showMessageBox("Nice job! you got it!")
                 self.TextInput.clear()
                 self.shuffle()
@@ -102,12 +109,9 @@ class PlayWidget(QtWidgets.QWidget):
         QtWidgets.QMessageBox.information(self, "Scales", value)
 
 
-        
-
-
 class HomeWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(HomeWidget, self).__init__(parent)
 
         self.learnButton = QtWidgets.QPushButton("Learn")
         self.learnButton.setStyleSheet("color: green; background-color: rgb(59,48,57); height: 100px; width: 200px; font-size: 45px; border-radius: 20px;")
@@ -123,23 +127,36 @@ class HomeWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.playButton)
         self.setLayout(self.layout)
 
-        self.learnButton.clicked.connect(self.learnButtonClicked)
-        self.playButton.clicked.connect(self.playButtonClicked)
-        #self.button.clicked.connect(self.magic)
-    def learnButtonClicked(self):
-        print('learn')
 
-    def playButtonClicked(self):
-        print('play')
-    #def magic(self):
-        #self.text.setText(random.choice(self.hello))
-        #self.text.repaint()
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setGeometry(50, 50, 400, 450)
+        #self.setFixedSize(400, 450)
+        self.startHomeWidget()
+
+    def startHomeWidget(self):
+        self.HomeWidget = HomeWidget(self)
+        self.setWindowTitle("Scales - Home")
+        self.setCentralWidget(self.HomeWidget)
+        self.HomeWidget.playButton.clicked.connect(self.startPlayWidget)
+        self.show()
+
+    def startPlayWidget(self):
+        self.PlayWidget = PlayWidget(self)
+        self.setWindowTitle("Scales - Play")
+        self.setCentralWidget(self.PlayWidget)
+        self.show()
+
 
 if __name__ == "__main__":
+    
   app = QtWidgets.QApplication([])
-  widget = PlayWidget('-')
+  #widget = PlayWidget('-')
+  widget = MainWindow()
   widget.setStyleSheet("background-color: rgb(39,71,133);")
-  widget.resize(800,600)
+  widget.resize(1000,800)
   widget.show()
 
   sys.exit(app.exec_())
